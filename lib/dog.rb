f1344b76cc10e3e0ce4e75af286854b11e45c146
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-09 01:24:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
   # code goes here
+  attr_accessor :dog, :owner
+
+  @@all =[]
+
+  def intialize(name)
+    @dog = name
+    @owner = owner
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
