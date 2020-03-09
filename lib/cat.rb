2e5439e4ca65f9f29c1a341523e513842dda5de0
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-09 02:45:29 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat
   # code goes here
-  attr_accessor :owner
+  attr_accessor :mood, :owner
   attr_reader :name
 
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
   end
 end
